db3bac1aa67ce6b55c91c95bcc88b5d20aa2eba8
PierreMrt <merlet.pierre@outlook.com>
2021-05-12 11:39:58 +0200

    added tests

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class World:
         self.rules = {
             'die'    : [0, 1, 4, 5, 6, 7, 8],
             'survive': [2, 3],
-            'birth'  : [3]
+            'birth'  : [2]
             }
         self.initial_state = self.random_state()
     
@@ -51,12 +51,7 @@ class World:
         next_board = copy.deepcopy(board)
         for r in range(len(board)):
             for c in range(len(board[0])):
-                nb_neighbours = 0
-                for n in self.neighbours:
-                    try:
-                        nb_neighbours += board[r + n[0]][c + n[1]]
-                    except IndexError:
-                        pass
+                nb_neighbours = self.count_neighbours(r, c, board)
                 if board[r][c] == 1 and nb_neighbours in self.rules['survive']:
                     next_board[r][c] = 1
                 elif board[r][c] == 1 and nb_neighbours in self.rules['die']:
@@ -64,6 +59,16 @@ class World:
                 elif board[r][c] == 0 and nb_neighbours in self.rules['birth']:
                     next_board[r][c] = 1    
         return next_board
+    
+    def count_neighbours(self, r, c, board):
+        nb_neighbours = 0
+        for n in self.neighbours:
+            try:
+                nb_neighbours += board[r + n[0]][c + n[1]]
+            except IndexError:
+                pass
+        return nb_neighbours
+
 
     def run(self):
         board = copy.deepcopy(self.initial_state)
